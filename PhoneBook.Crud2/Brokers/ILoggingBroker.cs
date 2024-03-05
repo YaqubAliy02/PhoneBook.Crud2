7ffd3a78cb00390ae156fc83b59536ad5fa1848d
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-03-05 18:08:07 +0500

    Implement Logging

---
+++ PhoneBook.Crud2/Brokers/ILoggingBroker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PhoneBook.Crud2.Brokers
+{
+    internal interface ILoggingBroker
+    {
+        void LogInforamation(string message);
+        void LogError(Exception exception);
+
+    }
+}
