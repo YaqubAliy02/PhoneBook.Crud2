1f9a047a966e24df3a7e616ec3b311a35ebcfbcc
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-03-06 09:33:21 +0500

    Add Contact

--- PhoneBook.Crud2/Models/Contact.cs
+++ PhoneBook.Crud2/Models/Contact.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 namespace PhoneBook.Crud2.Models
 {
-    internal class Constact
+    internal class Contact
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
     }
 }
+
---
+++ PhoneBook.Crud2/Serivces/Contacts/ContactService.cs
@@ -0,0 +1,23 @@
+﻿using PhoneBook.Crud2.Brokers.Loggings;
+using PhoneBook.Crud2.Brokers.Loggings.Storages;
+using PhoneBook.Crud2.Models;
+using System;
+
+namespace PhoneBook.Crud2.Serivces.Contacts
+{
+    internal class ContactService : IContactService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        public ContactService()
+        {
+            this.storageBroker = new FileStorageBroker();
+            this.loggingBroker = new LoggingBroker();
+
+        }
+
+        // happy path
+        public Contact AddContact(Contact contact) =>
+          this.storageBroker.AddContact(contact);
+    }
+}
